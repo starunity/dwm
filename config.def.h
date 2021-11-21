c26e04aa39a39742e3c2674284785d9eb9eb247c
starunity <unity150574826@gmail.com>
2021-11-21 17:07:02 +0800

    fix client border color

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 9;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -22,7 +22,7 @@ static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
     /*                   fg         bg         border   */
     [SchemeNorm]     = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]      = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeSel]      = { col_gray4, col_cyan,  "#1793D1" },
     [SchemeStatus]   = { col_gray3, col_gray1, "#000000" }, // Statusbar right {text,background,not used but cannot be empty}
     [SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000" }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm] = { col_gray3, col_gray1, "#000000" }, // Tagbar left unselected {text,background,not used but cannot be empty}
